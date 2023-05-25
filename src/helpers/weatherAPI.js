cd3af05e288364b9548ca0849275983493280ee8
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-25 18:08:10 -0300

    requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,8 +1,18 @@
 // Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
+export const searchCities = async (term) => {
 //   seu código aquiaa
-
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+    const data = await response.json();
+    if (data.length === 0) {
+      return window.alert('Nenhuma cidade encontrada');
+    }
+    return console.log(data);
+  } catch (error) {
+    window.alert('Nenhuma cidade encontrada');
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
