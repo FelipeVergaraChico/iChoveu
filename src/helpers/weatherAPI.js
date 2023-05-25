2637d44a67d21cf12b6e055eda8914e302d6ac23
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-25 15:26:39 -0300

    começando

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,8 @@
 // Remova os comentários a medida que for implementando as funções
 
 export const searchCities = (/* term */) => {
-//   seu código aqui
+//   seu código aquiaa
+
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
